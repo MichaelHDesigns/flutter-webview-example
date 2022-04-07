29ef89759427a25b50648800078a05f1068075ed
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-04-07 13:49:30 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:qrcode/qrcode.dart';
 
 void main() => runApp(MyApp());
 
